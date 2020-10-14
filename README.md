716ca692214e9798d7cf778453de529a44142645
Raja s <rajaaltus@gmail.com>
2020-10-14 12:28:11 +0700

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# kottram Healthcare Private Limited
---
+++ pages/index.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="container mx-auto bg-gray-400 dark:bg-gray-800">
+    <h1>Welcome</h1>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
